e002c6e1bebff2eb5a1172d17116eca43c37bab4
rodri_000 <rodri_000@Neo.localdomain>
2017-10-01 16:43:13 -0300

    Criado o 'Hello World' Data: 01/10/2017.

---
+++ TesteGITHUB/src/testegithub/TesteGITHUB.java
@@ -0,0 +1,19 @@
+package testegithub;
+
+/**
+ *
+ * @author rodri_000
+ */
+public class TesteGITHUB {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        System.out.println("Hello GitHub");
+    }
+    
+}
