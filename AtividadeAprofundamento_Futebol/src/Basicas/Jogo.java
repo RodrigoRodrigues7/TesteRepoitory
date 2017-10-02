08c900a8fca075e06fb458c7e07300c1d8cf2974
Rodrigo Rodrigues <>
2017-10-02 19:13:31 -0300

    Mudança na classe 'Jogo'(adicionado o @GeneratedValue) Data: 02/10/2017.

--- AtividadeAprofundamento_Futebol/src/Basicas/Jogo.java
+++ AtividadeAprofundamento_Futebol/src/Basicas/Jogo.java
@@ -4,6 +4,8 @@ import java.util.Calendar;
 import java.util.Objects;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -16,6 +18,7 @@ import org.hibernate.annotations.FetchMode;
 public class Jogo {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @ManyToOne(fetch = FetchType.EAGER)
