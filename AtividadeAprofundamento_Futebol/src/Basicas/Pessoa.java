8620e7ef54e395b762ab84196ed0bd385d403064
Rodrigo Rodrigues <>
2017-10-02 18:01:54 -0300

    Tornado abstrato a classe 'Pessoa' Data: 02/10/2017.

--- AtividadeAprofundamento_Futebol/src/Basicas/Pessoa.java
+++ AtividadeAprofundamento_Futebol/src/Basicas/Pessoa.java
@@ -1,19 +1,16 @@
 package Basicas;
 
 import java.util.Calendar;
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
+import javax.persistence.MappedSuperclass;
 
-@Entity
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
-public class Pessoa {
+@MappedSuperclass
+public abstract class Pessoa {
     
     @Id
-    @GeneratedValue(strategy = GenerationType.TABLE)//<------
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String nome;
     private Calendar dataNasc;
