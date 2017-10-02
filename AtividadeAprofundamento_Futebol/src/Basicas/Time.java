e2aa5fbf0f0654dfdb29f424a08542a074ede62b
Rodrigo Rodrigues <>
2017-10-02 17:12:12 -0300

    Alteração na classe 'Time'(Annotation @ManyToOne) Data: 02/10/2017.

--- AtividadeAprofundamento_Futebol/src/Basicas/Time.java
+++ AtividadeAprofundamento_Futebol/src/Basicas/Time.java
@@ -6,10 +6,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import org.hibernate.annotations.Cascade;
 import org.hibernate.annotations.CascadeType;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 @Entity
 public class Time {
@@ -21,12 +24,15 @@ public class Time {
     private String estado;
     private int pontos;
     
-    @OneToMany(mappedBy="time")
+    @OneToMany
     @Cascade(CascadeType.ALL)
     private List<Jogador> jogadores;
     
-    @OneToOne
-    @Cascade(CascadeType.ALL)
+    @ManyToOne
+    @JoinColumn(name = "FK_ID_Tecnico",
+            insertable = true, updatable = true)
+    @Fetch(FetchMode.JOIN)
+    @Cascade(CascadeType.SAVE_UPDATE)
     private Tecnico tecnico;
     
     @OneToMany(mappedBy = "timeCasa")
