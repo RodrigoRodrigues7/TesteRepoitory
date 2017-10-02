e19bca78457d5d9e8192c5807b797b3243825809
Rodrigo Rodrigues <>
2017-10-02 18:05:07 -0300

    Mudança da annotation '@ManyToOne' para '@OnetoOne'; Adicionado a annotation '@Cascade' e mudado o 'mappedBy'; Data: 02/10/2017.

--- AtividadeAprofundamento_Futebol/src/Basicas/Tecnico.java
+++ AtividadeAprofundamento_Futebol/src/Basicas/Tecnico.java
@@ -1,14 +1,14 @@
 package Basicas;
 
 import javax.persistence.Entity;
-import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import org.hibernate.annotations.Cascade;
 import org.hibernate.annotations.CascadeType;
 
 @Entity
 public class Tecnico extends Pessoa {
 
-    @OneToMany(mappedBy = "tecnico")
+    @OneToOne(mappedBy = "tecnico")
     @Cascade(CascadeType.ALL)
     private Time time;
 
