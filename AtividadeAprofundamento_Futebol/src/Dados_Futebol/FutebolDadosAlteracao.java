3da3baccc061f030861b3559f453c8aa409380d2
Rodrigo Rodrigues <>
2017-10-03 16:56:59 -0300

    Métodos do 'Main': 'FutebolDadosAlteracao' adicionados Data: 03/10/2017.

--- AtividadeAprofundamento_Futebol/src/Dados_Futebol/FutebolDadosAlteracao.java
+++ AtividadeAprofundamento_Futebol/src/Dados_Futebol/FutebolDadosAlteracao.java
@@ -1,5 +1,8 @@
 package Dados_Futebol;
 
+import Basicas.Jogador;
+import Basicas.Jogo;
+import Basicas.Time;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
@@ -8,20 +11,31 @@ import javax.persistence.Persistence;
 public class FutebolDadosAlteracao {
 
     public static void main(String[] args) {
-        
+
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hibernate_futebol");
         EntityManager em = emf.createEntityManager();
-        
+
         EntityTransaction et = em.getTransaction();
         et.begin();
         
+        /*
+        Jogador ilsinho = em.find(Jogador.class, new Integer(6));
+        ilsinho.setNome("Ilsinho Cavalcanti");
+        ilsinho.setSalario(89000.00);
+        ilsinho.setTemCardAmarelo(true);
+        */
         
+        Jogador alexSilva = em.find(Jogador.class, new Integer(2));
+        
+        
+        //em.persist(ilsinho);
+        em.persist(alexSilva);
         
         et.commit();
-        
+
         em.close();
         emf.close();
-        
+
     }
 
 }
