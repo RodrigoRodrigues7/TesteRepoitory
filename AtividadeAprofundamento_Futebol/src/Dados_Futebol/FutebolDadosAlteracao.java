7046bc2d4a0c9ea5dee9be58947cefb68a71356d
rodri_000 <rodri_000@Neo.localdomain>
2017-10-04 02:46:59 -0300

    Alterações no método Main 'FutebolDadosAlteracao' Data: 04/10/2017.

--- AtividadeAprofundamento_Futebol/src/Dados_Futebol/FutebolDadosAlteracao.java
+++ AtividadeAprofundamento_Futebol/src/Dados_Futebol/FutebolDadosAlteracao.java
@@ -2,6 +2,7 @@ package Dados_Futebol;
 
 import Basicas.Jogador;
 import Basicas.Jogo;
+import Basicas.Tecnico;
 import Basicas.Time;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityManager;
@@ -17,33 +18,30 @@ public class FutebolDadosAlteracao {
 
         EntityTransaction et = em.getTransaction();
         et.begin();
-
+//
         Jogador ilsinho = em.find(Jogador.class, new Integer(6));
         ilsinho.setNome("Ilsinho Cavalcanti");
         ilsinho.setSalario(89000.00);
         ilsinho.setTemCardAmarelo(true);
+//        
+//===============================================================
 //
         Jogador alexSilva = em.find(Jogador.class, new Integer(2));
         alexSilva.setTime(em.getReference(Time.class, new Integer(3)));
+//        
+//===============================================================
 //
         Jogador rogerioCeni = em.find(Jogador.class, new Integer(1));
-
 //
         Time cruzeiro = em.find(Time.class, new Integer(2));
-        Jogador edCarlos = em.find(Jogador.class, new Integer(4));
-        Jogador fabao = em.find(Jogador.class, new Integer(3));
-
 //        
 //===============================================================
 //
         em.remove(rogerioCeni);
         em.remove(cruzeiro);
-        em.remove(edCarlos);
-        em.remove(fabao);
 
         em.persist(ilsinho);
         em.persist(alexSilva);
-
         et.commit();
 
         em.close();
